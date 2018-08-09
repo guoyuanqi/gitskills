0c02b5507f8474d39d5124b6ac101585ffef82b5
guoyuanqi <670099830@qq.com>
2018-08-09 14:41:06 +0800

    创建Maven项目

---
+++ src/main/java/com/baizhi/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+/**
+ * java类简单作用描述
+ *
+ * @ClassName: HelloWorld
+ * @Description: ${TODO}
+ * @author: guoyuanqi
+ * @date: 2018/8/914:33
+ */
+public class HelloWorld {
+
+}
